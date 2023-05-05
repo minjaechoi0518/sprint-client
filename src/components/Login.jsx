afda25c133c02c3bb7a3799a3a848a8c7face521
stella0905 <o122233@naver.com>
2023-05-06 03:17:29 +0900

    Feat 로그인 기능 구현

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,8 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
+import useInput from './Hooks/useInput'
 
 const Login = () => {
+  const [userId, onChangeUserIdHandler ] = useInput('')
+  const [password, onChangPasswordHandler] = useInput('')
+
+  const [warningNotice, setWarningNotice] = useState('')
+
+  const onClickLoginHandler = ()=>{
+    if(userId.length <1 ){
+      setWarningNotice('아이디를 입력해주세요')
+      return;
+    }
+    if(password.length <1 ){
+      setWarningNotice('비밀번호를 입력해주세요')
+      return;
+    }
+
+  }
   return (
-    <div>Login</div>
+    <>
+    <h1>로그인</h1>
+    <form>
+    <input value={userId} onChange={onChangeUserIdHandler}/>
+    <input value={password} onChange={onChangPasswordHandler}/>
+    </form>
+    <button onClick={onClickLoginHandler}>로그인</button>
+    {warningNotice}
+
+
+    <div>
+    아이디가 없으신가요?<Link to='/signUp'>회원가입</Link>
+    </div>
+    </>
   )
 }
 

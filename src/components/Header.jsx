52d2bdaf3224f8b163e3c02f63f7f7275633a1a6
stella0905 <109677566+stella0905@users.noreply.github.com>
2023-05-06 18:18:13 +0900

    Revert "Hamberber button "

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,21 +1,13 @@
-import React from "react";
-import * as CSS from "./component/style";
-import MenuBar from "./MenuBar";
+import React from 'react'
+import * as CSS from './component/style'
 
-// import hanmbergerIcon from "./public/hanmbergerIcon.png";
 
 const Header = () => {
-  "./MenuBar";
   return (
-    <div>
-      <CSS.Header>
-        <CSS.HeaderTitle>SPRINT</CSS.HeaderTitle>
-      </CSS.Header>
-      <div>
-        <MenuBar />
-      </div>
-    </div>
-  );
-};
+    <CSS.Header>
+    <CSS.HeaderTitle>SPRINT</CSS.HeaderTitle>
+    </CSS.Header>
+  )
+}
 
-export default Header;
+export default Header
--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,88 +1,9 @@
-import React, { useState } from "react";
-import styled from "styled-components";
+import React from 'react'
 
-function MenuBar() {
-  const [isActive, setIsActive] = useState(false);
-
-  const toggleMenu = () => {
-    setIsActive(!isActive);
-  };
-
+const MenuBar = () => {
   return (
-    <MenuBarContainer>
-      <HamburgerButton className="hamburger" onClick={toggleMenu}>
-        <HamburgerLine />
-        <HamburgerLine />
-        <HamburgerLine />
-      </HamburgerButton>
-      <MenuBarDropdown className={`menu-bar ${isActive ? "active" : ""}`}>
-        <ul>
-          <li>
-            <a href="#">메뉴1</a>
-          </li>
-          <li>
-            <a href="#">메뉴2</a>
-          </li>
-          <li>
-            <a href="#">메뉴3</a>
-          </li>
-        </ul>
-      </MenuBarDropdown>
-    </MenuBarContainer>
-  );
+    <div>MenuBar</div>
+  )
 }
 
-export default MenuBar;
-
-const MenuBarContainer = styled.div`
-  position: relative;
-`;
-
-const HamburgerButton = styled.button`
-  display: inline-block;
-  background-color: transparent;
-  border: none;
-  cursor: pointer;
-`;
-
-const HamburgerLine = styled.span`
-  display: block;
-  width: 25px;
-  height: 3px;
-  margin: 5px 0;
-  background-color: #333;
-`;
-
-const MenuBarDropdown = styled.div`
-  position: absolute;
-  top: 100%;
-  left: 0;
-  display: none;
-
-  ${MenuBarContainer}.active & {
-    display: block;
-  }
-
-  ul {
-    list-style: none;
-    padding: 0;
-    margin: 0;
-  }
-
-  li {
-    margin-bottom: 10px;
-  }
-
-  a {
-    display: block;
-    padding: 10px;
-    text-decoration: none;
-    color: #333;
-    border-radius: 3px;
-    transition: background-color 0.3s ease;
-
-    &:hover {
-      background-color: #eee;
-    }
-  }
-`;
+export default MenuBar
